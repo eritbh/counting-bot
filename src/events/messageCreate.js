0bee5f112c8ef9859d1e5f43ae8a2774a7e96198
Erin <erin20913@gmail.com>
2021-04-18 23:11:29 -0400

    split out number parsing for consistency, etc

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,5 +1,17 @@
 const {EventListener} = require('yuuko');
 
+function getNumberFromMessage (message) {
+	const numberPart = message.content.match(/^\d+$/);
+	if (!numberPart) {
+		return NaN;
+	}
+	const num = parseFloat(numberPart[0]);
+	if (isNaN(num)) {
+		return NaN;
+	}
+	return num;
+}
+
 module.exports = new EventListener('messageCreate', async (message, context) => {
 	// Ignore messages not in the counting channel
 	if (!message.guildID || message.channel.name !== 'counting') {
@@ -11,43 +23,28 @@ module.exports = new EventListener('messageCreate', async (message, context) => 
 		return;
 	}
 
-	// Only care about messages with numbers in them
-	let numberPart = message.content.match(/\d+/);
-	if (!numberPart) {
-		return;
-	}
-	numberPart = parseFloat(numberPart[0]);
-	if (isNaN(numberPart)) {
-		return;
-	}
+	// Only care about messages that are only numbers
+	const number = getNumberFromMessage(message);
+	if (isNaN(number)) return;
 
 	// Get the latest messages from the channel and find the last one that was counting
 	const oldMessages = await message.channel.getMessages();
 	oldMessages.splice(0, oldMessages.findIndex(m => m.id === message.id) + 1);
 	// eslint-disable-next-line prefer-const,no-unused-vars
-	let [lastAuthor, lastNumber] = oldMessages.map(m => [m.author.id, parseInt(m.content.replace(/[,.]/g, ''), 10)]).filter(([author, num]) => !isNaN(num))[0] || [null, null];
+	let [lastAuthor, lastNumber] = oldMessages.map(m => [m.author.id, getNumberFromMessage(m)]).find(([author, num]) => !isNaN(num)) || [null, null];
 	if (lastNumber == null) {
 		lastNumber = 0;
 	}
 
-	const numberIsCorrect = numberPart === lastNumber + 1;
-	const isOnlyNumber = !!message.content.match(/^\d+$/);
-	const isLastAuthor = message.author.id === lastAuthor;
-
-	if (numberIsCorrect && (!isOnlyNumber || isLastAuthor) || !numberIsCorrect && isOnlyNumber) {
-		// if the number is right but has other text, delete it
+	if (number !== lastNumber + 1 || message.author.id === lastAuthor) {
 		// if the number is right but was sent by the same person who sent the previous number, delete it
 		// if the number is wrong and doesn't have other text, delete it
 		message.delete().catch(error => {
 			console.error('error deleting bad number:', error);
 		});
-	} else if (numberIsCorrect) {
-		// if the number is right and doesn't have other text, approve it
+	} else {
 		message.addReaction(context.getEmoji(message.channel.id)).catch(error => {
 			console.error('error adding reaction to good number:', error);
 		});
-	} else {
-		// if the number is wrong but has other text, let it vibe
-		console.log('weird condition:', message.content, lastNumber);
 	}
 });
